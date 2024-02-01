d2586795d4c358ab82693114798ab8d2fc2168b7
Lu Xinming <xinmingsm11@gmail.com>
2024-02-01 22:29:55 +0800

    updated time zone

--- atlas_3/atlas_3/settings.py
+++ atlas_3/atlas_3/settings.py
@@ -37,11 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
+    "ninja"
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -109,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC+8"
+TIME_ZONE = "Asia/Singapore"
 
 USE_I18N = True
 
