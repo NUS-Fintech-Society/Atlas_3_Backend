53342db9857bdf066a3c2f1bbd68f1dac57754c5
Lu Xinming <xinmingsm11@gmail.com>
2024-02-05 00:57:38 +0800

    refactored csrf endpoint to /api/auth

--- atlas_3/auth/api.py
+++ atlas_3/auth/api.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+
 from ninja import Router
 from ninja.security import django_auth
 
@@ -7,3 +10,10 @@ router = Router()
 @router.get("/private_data", auth=django_auth)
 def private_data(request):
     return {"data": "Private data"}
+
+
+@router.post("/csrf")
+@ensure_csrf_cookie
+@csrf_exempt
+def get_csrf_token(request):
+    return HttpResponse()
