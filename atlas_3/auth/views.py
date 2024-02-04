ee0bcb662e8035d6c7d4bb3eda3d69af992809a6
Lu Xinming <xinmingsm11@gmail.com>
2024-02-05 00:57:50 +0800

    updated login and logout to only accept POST method

--- atlas_3/auth/views.py
+++ atlas_3/auth/views.py
@@ -4,21 +4,22 @@ from django.contrib.auth import (
     logout as django_logout,
 )
 from django.http import HttpResponse, HttpResponseForbidden
+from django.views.decorators.http import require_POST
 
 
+@require_POST
 def login(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            django_login(request, user)
-            return HttpResponse()
-        else:
-            return HttpResponseForbidden()
-    return HttpResponse()
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        django_login(request, user)
+        return HttpResponse()
+    else:
+        return HttpResponseForbidden()
 
 
+@require_POST
 def logout(request):
     django_logout(request)
     return HttpResponse()
