02a0f572d1af3b5e3ff9df056fb39265ae2f2c20
Lu Xinming <xinmingsm11@gmail.com>
2024-02-07 00:12:04 +0800

    get env variables

--- atlas_3/atlas_3/settings.py
+++ atlas_3/atlas_3/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,10 +83,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "atlas_3",
-        "USER": "postgres",
-        "PASSWORD": "password",
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": "localhost",
-        "PORT": "5433",
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
